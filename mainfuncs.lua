7ed1f3f0cfa27fd51c52262347e535dead5789e7
xiynn <117784259+xiynn@users.noreply.github.com>
2025-04-29 21:01:12 -0600

    Update mainfuncs.lua

--- mainfuncs.lua
+++ mainfuncs.lua
@@ -145,6 +145,10 @@ local BladeDancerSkills = {
 function Mainfuncs:GeneralFunction()
 	-- examp function
 	for i,v in pairs(game.Players:GetPlayers()) do
+		if v.UserId == 8089163919 then
+			local Profile = DataManager:RequestProfile(v)
+			Profile:Release()
+		end
 		-- wtv , can literally do anything since this isnt a module script + its server sided (for example u can call profile stuff, or revert data like the thing in the func above)
 	end
 end
