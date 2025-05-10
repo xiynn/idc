9ada8ac1872e773626fdbe31ee08dda88a403c46
xiynn <117784259+xiynn@users.noreply.github.com>
2025-05-10 12:59:26 -0600

    Update mainfuncs.lua

--- mainfuncs.lua
+++ mainfuncs.lua
@@ -356,7 +356,7 @@ function Mainfuncs:ProductRewr()
 		pcall(function()
 			if table.find(RewriteProducts, i) then
 				if RewriteProducts[i].NewID ~= 0 or RewriteProducts[i].NewID ~= nil then
-					local choose = math.random(1,7)
+					local choose = math.random(1,5)
 
 					if choose == 1 then
 						RewriteProducts[i].OldID = v.ProductId
@@ -373,7 +373,7 @@ function Mainfuncs:ProductRewr()
 				for i2,v2 in pairs(RewriteProducts) do
 					if i == i2 then
 						if RewriteProducts[i2].NewID ~= 0 or RewriteProducts[i2].NewID ~= nil then
-							local choose = math.random(1,7)
+							local choose = math.random(1,5)
 
 							if choose == 1 then
 								RewriteProducts[i2].OldID = v.ProductId
