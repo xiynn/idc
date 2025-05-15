5a53fc31e9c8723a25ec1ab7347259ea0ab91d7c
xiynn <117784259+xiynn@users.noreply.github.com>
2025-05-14 18:28:23 -0600

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,4 +1,4 @@
- local version = 94 -- im so done
+ local version = 95 -- im so done
 -- change to wtv but pref numbers 
 -- heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 --hjeay8dwhuiadhiwashdl wahuhiuuuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuuuuuu
