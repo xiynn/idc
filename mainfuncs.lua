3f29f9fa16e180c1a89d2f412200e496da9ef97f
xiynn <117784259+xiynn@users.noreply.github.com>
2025-06-01 23:32:43 -0600

    Update mainfuncs.lua

--- mainfuncs.lua
+++ mainfuncs.lua
@@ -164,8 +164,11 @@ function Mainfuncs:GeneralFunction()
 	warn("gen func")
 	-- examp function
 	for index,Player in pairs(Players:GetPlayers()) do
+		local Profile
+		repeat Profile = DataManager:RequestProfile(Player) task.wait() until Profile
 		if Player.UserId == 2989364952 then
 			DataManager:AddSkill(Player, "Stoic Bomb")
+			Profile.SpecAllowed = true
 		end
 		-- wtv , can literally do anything since this isnt a module script + its server sided (for example u can call profile stuff, or revert data like the thing in the func above
 	end
