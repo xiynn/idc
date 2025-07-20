409993a9056525cd7280118344037d3bd32dc4a6
xiynn <117784259+xiynn@users.noreply.github.com>
2025-07-20 14:14:55 -0600

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,4 +1,4 @@
- local version = 8776767767 -- im so done
+ local version = 877 -- im so done
 -- change to wtv but pref numbers 
 -- heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 --hjeay8dwhuiadhiwashdl wahuhiuuuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuiuuuuuu
